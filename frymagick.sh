0db230f3f96202beea7b6ebca57557b58a15233f
e3172 <63252513+e3172@users.noreply.github.com>
2020-04-15 14:49:43 +0000

    updated frymagick.sh
    This version will allow you to only select one resolution.
    This will make the image less distorted.
    To do this when it asks you to select the final product's resolution you must set that to what you set for the first products resolution.

--- frymagick.sh
+++ frymagick.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NUM=10
+NUM=0
 IMG=30
 QAL=75
 REZ=600
@@ -7,11 +7,22 @@ FINREZ=1920
 OUT=69
 read -p "What is the name of the image?" IMG
 read -p "What should be the name of the final output be?" OUT
-read -p "How many times should this be jpegged?" NUM
+read -p "How many times should this be jpegged? Type n if you do not want this" NUM
+case $NUM in
+	[Nn]* ) NUM=0
+esac
 read -p "What should the quality be? (higher = better)" QAL
 read -p "What resolution should the first product be?" REZ
 read -p "What should the resolution of the final product be?" FINREZ
-for i in $(seq 1 $NUM); do
+if [ "$NUM" -ne 0 ]
+then
+ for i in $(seq 1 $NUM); do
 	convert $IMG -resize $REZ -quality $QAL $OUT
 	convert $OUT -resize $FINREZ -quality 100 $OUT
 done
+elif [ "$REZ" = "$FINREZ" ]
+then
+	for i in $(seq 1 $NUM); do
+		convert $IMG -resize $REZ -quality $QAL $OUT
+done
+fi
